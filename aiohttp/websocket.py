963291af17ee9ef0367526760814680829e75061
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-07 01:44:44 -0500

    Counting task keeps going, sends messages to all websockets

--- aiohttp/websocket.py
+++ aiohttp/websocket.py
@@ -5,6 +5,9 @@ from concurrent.futures import CancelledError
 
 
 async def index(request):
+    app = request.app
+    if app['count_task'] is None or app['count_task'].done():
+        app['count_task'] = asyncio.ensure_future(count(app, 20))
     with (Path(__file__).parent / 'index.html').open('rb') as fp:
         return web.Response(body=fp.read())
 
@@ -17,12 +20,8 @@ async def websocket_handler(request):
 
     request.app['sockets'].add(resp)
 
-    task = asyncio.ensure_future(count(resp, 8))
     async for msg in resp:
         print(msg.data)
-
-    if not task.done():
-        task.cancel()
 
     await resp.close()
 
@@ -32,25 +31,22 @@ async def websocket_handler(request):
     return resp
 
 
-async def count(ws, n):
+async def count(app, n):
     """
-    Send 1 to n numbers to the given websocket.
+    Send 1 to n numbers to all the websockets.
 
     """
     for i in range(1, n+1):
-        try:
+        print('Count: %d' % i)
+        await asyncio.sleep(1.0)
+        for ws in app['sockets']:
             ws.send_str(str(i))
-            await asyncio.sleep(1.0)
-        except CancelledError:
-            print('Count was cancelled at %d' % i)
-            return
-
-    await ws.close()
 
 
 def main():
     app = web.Application()
     app['sockets'] = set()
+    app['count_task'] = None
     app.router.add_route('GET', '/', index)
     app.router.add_route('GET', '/websocket/', websocket_handler)
     app.router.add_static('/static/', Path(__file__).parent)
