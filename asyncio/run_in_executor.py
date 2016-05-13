a5bfd644200e7840445e21467c36064fe6815817
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-12 19:45:59 -0500

    Synchronous code in child thread communicates back to main thread's event loop

--- asyncio/run_in_executor.py
+++ asyncio/run_in_executor.py
@@ -9,28 +9,24 @@ def synchronous_function():
     for i in range(COUNT):
         print(i)
         time.sleep(1)
-    return 'Hoosier Mama'
 
 
 def synchronous_generator_function():
     for i in range(65, 65 + COUNT):
         yield chr(i)
         time.sleep(1)
-        
+
 
 async def func1():
-    message = await loop.run_in_executor(None, synchronous_function)
-    print('The secret message is:', message)
+    await loop.run_in_executor(None, synchronous_function)
 
 
 async def func2():
-    generator = synchronous_generator_function()
-    while True:
-        try:
-            value = await loop.run_in_executor(None, lambda: next(generator))
-            print(value)
-        except StopIteration:
-            break
+    def generator_function_wrapper(fn):
+        for v in fn():
+            loop.call_soon_threadsafe(lambda: print(v + ' in bed!'))
+
+    await loop.run_in_executor(None, generator_function_wrapper, synchronous_generator_function)
 
 
 async def main():
