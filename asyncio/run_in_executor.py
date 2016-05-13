3872f70619703c3a22dda1ddfdad58355b687e1d
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-12 19:06:04 -0500

    Run two coroutines side-by-side

--- asyncio/run_in_executor.py
+++ asyncio/run_in_executor.py
@@ -1,17 +1,30 @@
 import asyncio
 
 
+COUNT = 5
+
+
 def synchronous_function():
     import time
-    for i in range(4):
+    for i in range(COUNT):
         print(i)
-        time.sleep(1.0)
+        time.sleep(1)
     return 'Hoosier Mama'
 
 
-async def main():
+async def func1():
     message = await loop.run_in_executor(None, synchronous_function)
     print('The secret message is:', message)
+
+
+async def func2():
+    for i in range(65, 65 + COUNT):
+        print(chr(i))
+        await asyncio.sleep(1)
+
+
+async def main():
+    await asyncio.wait([func1(), func2()])
 
 
 loop = asyncio.get_event_loop()
