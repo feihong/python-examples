96f4bb049e7a78aedd834fb4c3e0c4a8040e793a
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-13 13:58:22 -0500

    Refactor generator task example for clarity

--- aiohttp/run_tasks_sync/app.py
+++ aiohttp/run_tasks_sync/app.py
@@ -5,21 +5,17 @@ While a given task is running, it sends messages to all active websockets.
 """
 
 import sys
-import time
 import asyncio
 import functools
 import threading
 from pathlib import Path
 from aiohttp import web
-from mako.template import Template
 
 
 class GeneratorTask:
-    def __init__(self, name, fn, callback):
+    def __init__(self, name):
         self.name = name
-        self.fn = fn
         self.stop_event = threading.Event()
-        self.callback = callback
 
     def cancel(self):
         self.stop_event.set()
@@ -27,46 +23,48 @@ class GeneratorTask:
     def done(self):
         return self.stop_event.is_set()
 
+    def start(self):
+        loop = asyncio.get_event_loop()
+        coroutine = loop.run_in_executor(None, self._stoppable_run)
+        self.task = asyncio.ensure_future(coroutine)
+
     def run(self):
-        self.task = asyncio.ensure_future(self._run())
+        """
+        Override with generator method.
+
+        """
+        raise NotImplemented
 
     def add_done_callback(self, callback):
         self.task.add_done_callback(callback)
 
-    async def _run(self):
+    def _stoppable_run(self):
+        for value in self.run():
+            if self.stop_event.is_set():
+                break
+        self.stop_event.set()
+
+
+class AwesomeTask(GeneratorTask):
+    def __init__(self, name, count, logger):
+        super(AwesomeTask, self).__init__(name)
+        self.count = count
         loop = asyncio.get_event_loop()
-        threadsafe_callback = functools.partial(loop.call_soon_threadsafe, self.callback)
+        self.log = functools.partial(loop.call_soon_threadsafe, logger.info)
 
-        def wrapper():
-            for value in self.fn():
-                if self.stop_event.is_set():
-                    break
-                time.sleep(1)   # to simulate a computation-intensiveness
-                threadsafe_callback(value)
-            self.stop_event.set()
-
-        await loop.run_in_executor(None, wrapper)
-
-
-def get_function_pairs(names):
-    project_root = Path(__file__).absolute().parents[2]
-    sys.path.append(str(project_root))
-    import generate
-
-    for name in names:
-        yield name, getattr(generate, name)
-
-
-FUNCTIONS = dict(get_function_pairs([
-    'generate_chinese_characters', 'generate_emoticons'
-]))
+    def run(self):
+        import random
+        import time
+        for i in range(65, 65 + self.count):
+            c = chr(i)
+            self.log(c * random.randint(10, 50))
+            time.sleep(1)
+            yield
 
 
 async def index(request):
     with (Path(__file__).parent / 'index.html').open() as fp:
-        html = Template(fp.read()).render(
-            function_names=FUNCTIONS.keys())
-        return web.Response(text=html, content_type='text/html')
+        return web.Response(text=fp.read(), content_type='text/html')
 
 
 async def start_task(request):
@@ -77,16 +75,17 @@ async def start_task(request):
     if app['current_task'] and not app['current_task'].done():
         return web.Response(text='task not started')
 
-    name = request.GET['name']
     count = int(request.GET['count'])
-    fn = functools.partial(FUNCTIONS[name], count)
-    task = GeneratorTask(name, fn, logger.info)
-    task.run()
+    task = AwesomeTask('AwesomeTask', count, logger)
+    task.start()
 
     app['current_task'] = task
     app['logger'].info('Started task: %s' % task.name)
-    app['current_task'].add_done_callback(
-        lambda f: logger.info('Task %s completed' % task.name))
+
+    def done(*args):
+        logger.info('Task %s completed' % task.name)
+        app['current_task'] = None
+    app['current_task'].add_done_callback(done)
     return web.Response(text='task started')
 
 
@@ -94,8 +93,8 @@ async def stop_task(request):
     app = request.app
     task = app['current_task']
     if task and not task.done():
+        app['logger'].info('Cancelling task %s...' % task.name)
         task.cancel()
-        app['logger'].info('Trying to cancel task %s' % task.name)
         return web.Response(text='task cancelled')
     else:
         return web.Response(text='task not cancelled')
