920d0c80bd6a26567f97654a665df23438a766a5
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-13 12:35:06 -0500

    Use a context manager to change where the arguments to print() go

--- change_sys_stdout.py
+++ change_sys_stdout.py
@@ -1,15 +1,28 @@
 import sys
+from contextlib import contextmanager
 from io import StringIO
 
 
-stdout = sys.stdout
+@contextmanager
+def patch_print(filelike_obj):
+    """
+    Context manager that changes where the arguments of print() go to.
+
+    """
+    stdout = sys.stdout
+    sys.stdout = filelike_obj
+    yield
+    sys.stdout = stdout
+
+
+print('Before using context manager')
+
 output = StringIO()
-print('Changing sys.stdout to StringIO object')
-sys.stdout = output
+with patch_print(output):
+    print('Cool beans')
+    print('Amaze balls')
+    print('Awesome sauce')
 
-print('what the dillio')
-print('are you seeing this?')
-
-sys.stdout = stdout
-print('done!')
+print('After using context manager')
+print('\nOutput:')
 print(output.getvalue())
