574dbc10a043e696963c3bea04e14dd15ea3757d
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-17 21:57:57 -0500

    Add websockets example

---
+++ tornado/websockets/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var ws = new WebSocket('ws://' + window.location.host + '/websocket/')
+
+ws.onopen = () => {
+  console.log('Websocket opened');
+}
+
+ws.onclose = () => {
+  console.log('Websocket closed');
+}
+
+ws.onmessage = (evt) => {
+  let p = $('<p></p>')
+  p.text(evt.data)
+  $('#messages').append(p)
+}
+
+$('button.start').on('click', function() {
+  $.get('/start/', (data) => {
+    console.log(data)
+  })
+})
+
+$('button.stop').on('click', function() {
+  $.get('/stop/', (data) => {
+    console.log(data)
+  })
+})
+
+$('button.close').on('click', function() {
+  ws.close()
+})
---
+++ tornado/websockets/app.py
@@ -0,0 +1,78 @@
+import random
+from pathlib import Path
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
+from tornado import gen
+from tornado.websocket import WebSocketHandler
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+class StartHandler(RequestHandler):
+    def get(self):
+        handle = IOLoop.current().add_callback(
+            generate_characters, self.application.logger)
+        print('Handle:', handle)
+        self.write('Started background task')
+
+
+class StopHandler(RequestHandler):
+    def get(self):
+        # IOLoop.current().spawn_callback(background_task)
+        self.write('Stopping background task...')
+
+
+class StatusHandler(WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+        self.application.sockets.add(self)
+
+    def on_close(self):
+        print("WebSocket closed")
+        self.application.sockets.remove(self)
+
+    def on_message(self, message):
+        pass
+
+
+@gen.coroutine
+def generate_characters(logger):
+    count = random.randint(2, 6)
+    logger.info('Preparing to generate %d characters' % count)
+
+    for i in range(count):
+        c = chr(random.randint(0x4e00, 0x9fff))
+        logger.info(c)
+        yield gen.sleep(1)
+
+    logger.info('Finished generating characters')
+
+
+class Logger:
+    def __init__(self, sockets):
+        self.sockets = sockets
+
+    def info(self, text):
+        for socket in self.sockets:
+            socket.write_message(text)
+
+
+if __name__ == '__main__':
+    settings = dict(
+        static_path=str(Path(__file__).parent.absolute())
+    )
+    app = Application([
+        (r'/', MainHandler),
+        (r'/start/', StartHandler),
+        (r'/stop/', StopHandler),
+        (r'/websocket/', StatusHandler),
+    ], **settings)
+    app.sockets = set()
+    app.logger = Logger(app.sockets)
+
+    app.listen(8000)
+    loop = IOLoop.current()
+    loop.start()
