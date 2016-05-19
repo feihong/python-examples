4df57c014599fc3722f9c1f8bd12d70eb7b7972f
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-18 19:16:42 -0500

    Add ability to stop  the coroutine

--- tornado/websockets/app.py
+++ tornado/websockets/app.py
@@ -4,6 +4,7 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
 from tornado import gen
 from tornado.websocket import WebSocketHandler
+from tornado.locks import Event
 
 
 class MainHandler(RequestHandler):
@@ -13,15 +14,29 @@ class MainHandler(RequestHandler):
 
 class StartHandler(RequestHandler):
     def get(self):
+        """
+        Note that unlike asyncio.ensure_future(), IOLoop.add_callback() does
+        not return an object that allows you cancel the coroutine. That's why
+        we pass in an tornado.locks.Event object, which can be used to tell
+        the coroutine that it should stop processing.
+
+        """
         count = int(self.get_query_argument('count', 5))
+        self.application.stop_event = Event()
         IOLoop.current().add_callback(
-            generate_characters, count, self.application.logger)
+            generate_characters,
+            count,
+            self.application.logger,
+            self.application.stop_event)
         self.write('Started background task')
 
 
 class StopHandler(RequestHandler):
     def get(self):
-        self.write('Stopping background task...')
+        if self.application.stop_event:
+            self.application.stop_event.set()
+            self.write('Stopping background task...')
+            self.application.logger.info('Stopping background task...')
 
 
 class StatusHandler(WebSocketHandler):
@@ -38,10 +53,12 @@ class StatusHandler(WebSocketHandler):
 
 
 @gen.coroutine
-def generate_characters(count, logger):
+def generate_characters(count, logger, stop_event):
     logger.info('Preparing to generate %d characters' % count)
 
     for i in range(count):
+        if stop_event.is_set():
+            break
         c = chr(random.randint(0x4e00, 0x9fff))
         logger.info(c)
         yield gen.sleep(1)
@@ -70,6 +87,7 @@ if __name__ == '__main__':
     ], **settings)
     app.sockets = set()
     app.logger = Logger(app.sockets)
+    app.stop_event = None
 
     app.listen(8000)
     loop = IOLoop.current()
