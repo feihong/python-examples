7b2082c82cbf35acbcba9261109eacae8f23c6ae
Feihong <feihong.hsu@gmail.com>
2016-06-04 21:33:16 -0500

    Add pyjade example

---
+++ pyjade/preprocess_mako.py
@@ -0,0 +1,24 @@
+from mako.template import Template
+from pyjade.ext.mako import preprocessor
+
+jade_source = """
+p
+    = greeting
+    ,
+    = guest_name
+    !
+
+- todo = ['laundry', 'call Amy back', 'write memoirs']
+
+ul
+    each item in todo
+        li= item
+
+"""
+
+tmpl = Template(jade_source, preprocessor=preprocessor)
+html = tmpl.render(
+    greeting='Good morning',
+    guest_name='Mr Bojangles',
+)
+print(html)
