038f7b5c64358c5ac1d500ce7a15c1021837d295
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-04 23:31:00 -0500

    Add a useful docstring

--- pyjade/mixed_inheritance/doit.py
+++ pyjade/mixed_inheritance/doit.py
@@ -1,3 +1,11 @@
+"""
+What happens if you try to make your Jade template inherit from a Mako template?
+Unfortunately, bad things. It doesn't work at all unless you use the Template
+constructor. Trying to include head.jade causes the actual code to be imported,
+not the rendered results. It doesn't seem that you can mix template languages
+very well.
+
+"""
 from mako.lookup import TemplateLookup, Template
 from pyjade.ext.mako import preprocessor
 
