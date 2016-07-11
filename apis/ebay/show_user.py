efbbd0418195f8b934a01930c8603d6354c230fe
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-11 11:30:06 -0500

    Consolidate eBay params  into a single environment variable

--- apis/ebay/show_user.py
+++ apis/ebay/show_user.py
@@ -3,11 +3,9 @@ import os
 from ebaysdk.trading import Connection as Trading
 
 
-params = os.environ['EBAY_PARAMS'].split(';')
-credentials = dict(appid=params[0], devid=params[1], certid=params[2], token=os.environ['EBAY_USER_TOKEN'], config_file=None)
+credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
 
-
-api = Trading(**credentials)
+api = Trading(config_file=None, **credentials)
 response = api.execute('GetUser', {})
 pprint(response.dict())
 # print(response.reply)
