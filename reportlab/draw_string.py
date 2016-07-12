018690db07907d365ef87560657e36171f7052c7
Feihong <feihong.hsu@gmail.com>
2016-07-12 17:03:01 -0500

    Figured out how to make drawString work with Chinese characters

--- reportlab/draw_string.py
+++ reportlab/draw_string.py
@@ -1,15 +1,23 @@
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.pdfgen.canvas import Canvas
+
+
+pdfmetrics.registerFont(UnicodeCIDFont('STSong-Light'))
 
 point = 1
 inch = 72
 
+
 c = Canvas('test.pdf', pagesize=(8.5 * inch, 11 * inch))
 c.setStrokeColorRGB(0,0,0)
 c.setFillColorRGB(0,0,0)
-c.setFont('Helvetica', 12 * point)
+# c.setFont('Helvetica', 12 * point)
 c.drawString(1*inch, 10*inch, 'Hello World')
 
-c.setFont('Courier', 32*point)
+c.setFont('STSong-Light', 32*point)
 c.drawString(3*inch, 9*inch, 'Poop')
+c.drawString(3*inch, 8*inch, '你好世界！')
 
+c.showPage()
 c.save()
