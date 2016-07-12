0168e8af5cf3c014ccec0ea3bcfcb69679e67e07
Feihong <feihong.hsu@gmail.com>
2016-07-12 18:24:51 -0500

    Simplify the draw_string example

--- reportlab/draw_string.py
+++ reportlab/draw_string.py
@@ -1,24 +1,24 @@
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.pdfgen.canvas import Canvas
 
-
-pdfmetrics.registerFont(UnicodeCIDFont('STSong-Light'))
 
 point = 1
 inch = 72
 
 
 with open('test.pdf', 'wb') as fp:
-    c = Canvas(fp, pagesize=(8.5 * inch, 11 * inch))
+    c = Canvas(fp, pagesize=(8.5*inch, 11*inch))
     c.setStrokeColorRGB(0,0,0)
     c.setFillColorRGB(0,0,0)
-    # c.setFont('Helvetica', 12 * point)
-    c.drawString(1*inch, 10*inch, 'Hello World')
+    c.setFont('Helvetica', 12 * point)
 
-    c.setFont('STSong-Light', 32*point)
-    c.drawString(3*inch, 9*inch, 'Poop')
-    c.drawString(3*inch, 8*inch, '你好世界！')
+    y = 10
+    for i in range(1, 33):
+        c.drawString(
+            1 * inch,
+            y * inch,
+            '%d. I must not fear. Fear is the mind killer.' % i
+        )
+        y -= 0.3
 
     c.showPage()
     c.save()
