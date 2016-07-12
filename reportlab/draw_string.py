a1b0350b37511deb5fc9a18050368406b18a32e1
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-12 17:15:57 -0500

    Pass file handle into Canvas constructor

--- reportlab/draw_string.py
+++ reportlab/draw_string.py
@@ -9,15 +9,16 @@ point = 1
 inch = 72
 
 
-c = Canvas('test.pdf', pagesize=(8.5 * inch, 11 * inch))
-c.setStrokeColorRGB(0,0,0)
-c.setFillColorRGB(0,0,0)
-# c.setFont('Helvetica', 12 * point)
-c.drawString(1*inch, 10*inch, 'Hello World')
+with open('test.pdf', 'wb') as fp:
+    c = Canvas(fp, pagesize=(8.5 * inch, 11 * inch))
+    c.setStrokeColorRGB(0,0,0)
+    c.setFillColorRGB(0,0,0)
+    # c.setFont('Helvetica', 12 * point)
+    c.drawString(1*inch, 10*inch, 'Hello World')
 
-c.setFont('STSong-Light', 32*point)
-c.drawString(3*inch, 9*inch, 'Poop')
-c.drawString(3*inch, 8*inch, '你好世界！')
+    c.setFont('STSong-Light', 32*point)
+    c.drawString(3*inch, 9*inch, 'Poop')
+    c.drawString(3*inch, 8*inch, '你好世界！')
 
-c.showPage()
-c.save()
+    c.showPage()
+    c.save()
