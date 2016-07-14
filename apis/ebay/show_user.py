82ab0c3c2ce3d5d9450f379e61294ae3e25d9547
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-14 10:21:12 -0500

    Print most important info in show_user.py

--- apis/ebay/show_user.py
+++ apis/ebay/show_user.py
@@ -8,4 +8,8 @@ credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_P
 api = Trading(config_file=None, **credentials)
 response = api.execute('GetUser', {})
 pprint(response.dict())
-# print(response.reply)
+print()
+user = response.reply.User
+print('UserID: ' + user.UserID)
+print('Email: ' + user.Email)
+print('Positive feedback %: ' + user.PositiveFeedbackPercent)
