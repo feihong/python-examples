47828ecc957a63fbf3b45523f582b06f7821d600
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-19 14:33:05 -0500

    Fetch and print out item specific values

--- apis/ebay/get_single_item.py
+++ apis/ebay/get_single_item.py
@@ -6,7 +6,15 @@ from ebaysdk.shopping import Connection as Shopping
 credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
 api = Shopping(config_file=None, **credentials)
 
-response = api.execute('GetSingleItem', {'ItemID': 171992581889})
+response = api.execute('GetSingleItem', {
+    'ItemID': 171992581889,
+    'IncludeSelector': 'ItemSpecifics'
+})
 # pprint(response.dict())
 
 item = response.reply.Item
+item_specifics = item.ItemSpecifics.NameValueList
+print('Title: ' + item.Title)
+print('Item specifics:')
+for spec in item_specifics:
+    print('- %s: %s' % (spec.Name, spec.Value))
