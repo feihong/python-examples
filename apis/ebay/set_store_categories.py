0e1b8960990f4ffe3ca47f3c109c4e1e75844edf
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-20 21:09:06 -0500

    Add SetStoreCategories example

---
+++ apis/ebay/set_store_categories.py
@@ -0,0 +1,25 @@
+import json
+import datetime
+from pprint import pprint
+import os
+from ebaysdk.trading import Connection as Trading
+
+
+credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
+api = Trading(config_file=None, **credentials)
+
+with open('categories.json') as fp:
+    categories = json.load(fp)
+
+with open('request.xml', 'w') as fp:
+    xml = api.build_request_data('SetStoreCategories', {
+        'Action': 'Add',
+        'StoreCategories': categories,
+    }, None)
+    fp.write(xml)
+
+# response = api.execute('SetStoreCategories', {
+#     'Action': 'Add',
+#     'StoreCategories': categories,
+# })
+# pprint(response.dict())
