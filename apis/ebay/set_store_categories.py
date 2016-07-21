b4d0069bd45b54e49ba98589d54857448821e235
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-20 21:23:22 -0500

    Fix format of request

--- apis/ebay/set_store_categories.py
+++ apis/ebay/set_store_categories.py
@@ -11,15 +11,19 @@ api = Trading(config_file=None, **credentials)
 with open('categories.json') as fp:
     categories = json.load(fp)
 
-with open('request.xml', 'w') as fp:
-    xml = api.build_request_data('SetStoreCategories', {
-        'Action': 'Add',
-        'StoreCategories': categories,
-    }, None)
-    fp.write(xml)
+# with open('request.xml', 'w') as fp:
+#     xml = api.build_request_data('SetStoreCategories', {
+#         'Action': 'Add',
+#         'StoreCategories': {
+#             'CustomCategory': categories
+#         }
+#     }, None)
+#     fp.write(xml)
 
-# response = api.execute('SetStoreCategories', {
-#     'Action': 'Add',
-#     'StoreCategories': categories,
-# })
-# pprint(response.dict())
+response = api.execute('SetStoreCategories', {
+    'Action': 'Add',
+    'StoreCategories': {
+        'CustomCategory': categories
+    }
+})
+pprint(response.dict())
