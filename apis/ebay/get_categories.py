03314f3a6b187707c2dc77d69ef2d316bfa23aee
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-21 11:15:41 -0500

    Print child categories in the right order

--- apis/ebay/get_categories.py
+++ apis/ebay/get_categories.py
@@ -14,14 +14,16 @@ api = Trading(config_file=None, **credentials)
 response = api.execute('GetStore', {
     'CategoryStructureOnly': True,
 })
-with open('categories.json', 'w') as fp:
-    json.dump(response.dict()['Store']['CustomCategories']['CustomCategory'], fp, indent=2)
+# with open('categories.json', 'w') as fp:
+#     json.dump(response.dict()['Store']['CustomCategories']['CustomCategory'], fp, indent=2)
 
 categories = response.reply.Store.CustomCategories.CustomCategory
 if hasattr(categories, '__iter__'):
     for cat in categories:
-        print(cat.Name)
-        for child in getattr(cat, 'ChildCategory', []):
-            print('- ' + child.Name)
+        print('%s %s => ' % (cat.Name, cat.CategoryID))
+        children = getattr(cat, 'ChildCategory', [])
+        children.sort(key=lambda x: int(x.Order))
+        for child in children:
+            print('- %s %s => ' % (child.Name, child.CategoryID))
             if hasattr(child, 'ChildCategory') and child.Name != '9 pc set':
                 import ipdb; ipdb.set_trace()
