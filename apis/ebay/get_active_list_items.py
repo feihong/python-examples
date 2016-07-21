c14d1b3cfa740e87abcd4a1102bb64c6a055e60d
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-21 11:38:13 -0500

    Use OutputSelector to limit the fields that are returned

--- apis/ebay/get_active_list_items.py
+++ apis/ebay/get_active_list_items.py
@@ -10,22 +10,27 @@ api = Trading(config_file=None, **credentials)
 response = api.execute('GetMyeBaySelling', {
     'ActiveList': {
         'Include': True
-    }
+    },
+    'OutputSelector': [
+        'ActiveList.PaginationResult',
+        'ActiveList.ItemArray.Item.Title',
+        'ActiveList.ItemArray.Item.BuyItNowPrice',
+    ]
 })
 with open('items.json', 'w') as fp:
     json.dump(response.dict(), fp, indent=2)
 
 
-items = response.reply.ActiveList.ItemArray.Item
-print('Got %d active list items' % len(items))
-
-pagination_result = response.reply.ActiveList.PaginationResult
-print('%s total items' % pagination_result.TotalNumberOfEntries)
-print('%s total pages' % pagination_result.TotalNumberOfPages)
-print()
-
-print('First item:')
-item = items[0]
-print(item.Title)
-print(item.ListingDetails.ViewItemURL)
-print(item.BuyItNowPrice.value)
+# items = response.reply.ActiveList.ItemArray.Item
+# print('Got %d active list items' % len(items))
+#
+# pagination_result = response.reply.ActiveList.PaginationResult
+# print('%s total items' % pagination_result.TotalNumberOfEntries)
+# print('%s total pages' % pagination_result.TotalNumberOfPages)
+# print()
+#
+# print('First item:')
+# item = items[0]
+# print(item.Title)
+# print(item.ListingDetails.ViewItemURL)
+# print(item.BuyItNowPrice.value)
