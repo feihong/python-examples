7e52ec01a0e62116a8b50375b9094602b0aba703
Feihong <feihong.hsu@gmail.com>
2016-07-21 20:21:55 -0500

    Print out the event IDs

--- apis/meetup/get_upcoming_events.py
+++ apis/meetup/get_upcoming_events.py
@@ -8,14 +8,14 @@ import arrow
 api_key = os.environ['MEETUP_API_KEY']
 
 
-url = 'https://api.meetup.com/%s/events?key=%s&status=upcoming' % (
-    'chinese-11', api_key)
+url = 'https://api.meetup.com/chinese-11/events?status=upcoming&key=' + api_key
 events = requests.get(url).json()
 
-with open('events.json', 'w') as fp:
-    json.dump(events, fp, indent=2)
+# with open('events.json', 'w') as fp:
+#     json.dump(events, fp, indent=2)
 
 for event in events:
+    print(event['id'])
     print(event['name'])
     dt = arrow.get(event['time'] / 1000.0).to('US/Central')
     print(dt)
