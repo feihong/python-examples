3663664af8e1203bd5154e4ed7e9ae122b106af4
Feihong Hsu <feihong.hsu@gmail.com>
2016-07-22 15:08:37 -0500

    Download both pages of results

--- apis/ebay/get_seller_list.py
+++ apis/ebay/get_seller_list.py
@@ -13,20 +13,35 @@ api = Trading(config_file=None, **credentials)
 now = arrow.utcnow()
 later = now.replace(days=+120)
 
-response = api.execute('GetSellerList', {
-    'EndTimeFrom': now,
-    'EndTimeTo': later,
-    'GranularityLevel': 'Medium',
-    'Pagination': {
-        'EntriesPerPage': 10,
-    },
-    'OutputSelector': [
-        'ReturnedItemCountActual',
-        'PaginationResult',
-        'ItemArray.Item.ItemID',
-        'ItemArray.Item.Title',
-        'ItemArray.Item.Storefront',
-    ]
-})
+def get_page(page):
+    response = api.execute('GetSellerList', {
+        'EndTimeFrom': now,
+        'EndTimeTo': later,
+        'GranularityLevel': 'Coarse',
+        'OutputSelector': [
+            'ReturnedItemCountActual',
+            'PaginationResult',
+            'ItemArray.Item.ItemID',
+            'ItemArray.Item.Title',
+            'ItemArray.Item.Storefront',
+        ],
+        'Pagination': {
+            'EntriesPerPage': 200,
+            'PageNumber': page,
+        },
+    })
+    return response.dict()
+
+
+def get_all_items():
+    page = get_page(1)
+    yield from page['ItemArray']['Item']
+    page_count = int(page['PaginationResult']['TotalNumberOfPages'])
+    for i in range(1, page_count):
+        page = get_page(i)
+        yield from page['ItemArray']['Item']
+
+
 with open('item_categories.json', 'w') as fp:
-    json.dump(response.dict(), fp, indent=2)
+    items = list(get_all_items())
+    json.dump(items, fp, indent=2)
