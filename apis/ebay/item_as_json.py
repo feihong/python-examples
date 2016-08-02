65e8a9d2dadc16be3dc592ed533e73713d7a6f09
Feihong <feihong.hsu@gmail.com>
2016-08-02 10:57:20 -0500

    Include ItemSpecifics value to item.json

--- apis/ebay/item_as_json.py
+++ apis/ebay/item_as_json.py
@@ -6,18 +6,28 @@ import json
 import os
 from pprint import pprint
 from ebaysdk.trading import Connection as Trading
+from ebaysdk.shopping import Connection as Shopping
 
 
-ITEM_ID = '142026412435'
+ITEM_ID = '141991824219'
 
 
 credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
-api = Trading(config_file=None, **credentials)
 
-response = api.execute('GetItem', {
+trading = Trading(config_file=None, **credentials)
+response = trading.execute('GetItem', {
     'ItemID': ITEM_ID,
     'DetailLevel': 'ReturnAll',
 })
+item = response.dict()
+
+shopping = Shopping(config_file=None, **credentials)
+response = shopping.execute('GetSingleItem', {
+    'ItemID': ITEM_ID,
+    'IncludeSelector': 'ItemSpecifics'
+})
+item['Item']['ItemSpecifics'] = response.dict()['Item']['ItemSpecifics']
+
 with open('item.json', 'w') as fp:
     json.dump(response.dict(), fp, indent=2)
 
