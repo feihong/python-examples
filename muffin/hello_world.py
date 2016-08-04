e33b72c8fa5fbd4e0c8410739a61e2a181c2d528
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-04 16:14:14 -0500

    Add muffin hello world example

---
+++ muffin/hello_world.py
@@ -0,0 +1,14 @@
+import muffin
+
+
+app = muffin.Application('example')
+
+
+@app.register('/', '/hello/{name}')
+def hello(request):
+    name = request.match_info.get('name', 'anonymous')
+    return 'Hello %s!' % name
+
+
+if __name__ == '__main__':
+    app.manage('run', '--bind=127.0.0.1:8000', '--reload')
