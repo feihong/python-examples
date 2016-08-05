299256c7bec2d9c08a991a283e32ea089b93684c
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-04 20:41:39 -0500

    Subclass muffin.urls.StaticRoute and override handle() method

--- muffin/static/main.py
+++ muffin/static/main.py
@@ -1,4 +1,8 @@
 import subprocess
+import mimetypes
+
+import asyncio
+from aiohttp.web_exceptions import HTTPNotFound, HTTPNotModified
 import muffin
 from muffin.urls import StaticRoute, StaticResource
 
@@ -11,7 +15,55 @@ def hello(request):
     return 'Hello World!'
 
 
-route = StaticRoute(None, '/', '.')
+class CustomStaticRoute(StaticRoute):
+    @asyncio.coroutine
+    def handle(self, request):
+        filename = request.match_info['filename']
+        try:
+            filepath = self._directory.joinpath(filename).resolve()
+            filepath.relative_to(self._directory)
+        except (ValueError, FileNotFoundError) as error:
+            # relatively safe
+            raise HTTPNotFound() from error
+        except Exception as error:
+            # perm error or other kind!
+            request.logger.exception(error)
+            raise HTTPNotFound() from error
+
+        st = filepath.stat()
+
+        modsince = request.if_modified_since
+        if modsince is not None and st.st_mtime <= modsince.timestamp():
+            raise HTTPNotModified()
+
+        ct, encoding = mimetypes.guess_type(str(filepath))
+        if not ct:
+            ct = 'application/octet-stream'
+
+        resp = self._response_factory()
+        resp.content_type = ct
+        if encoding:
+            resp.headers[hdrs.CONTENT_ENCODING] = encoding
+        resp.last_modified = st.st_mtime
+
+        file_size = st.st_size
+
+        resp.content_length = file_size
+        resp.set_tcp_cork(True)
+        try:
+            yield from resp.prepare(request)
+
+            with filepath.open('rb') as f:
+                yield from self._sendfile(request, resp, f, file_size)
+
+        finally:
+            resp.set_tcp_nodelay(True)
+
+        return resp
+
+
+
+route = CustomStaticRoute(None, '/', '.')
 resource = StaticResource(route)
 app.router._reg_resource(resource)
 
