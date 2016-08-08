65f7e8a61bc3bb2d94966962359f0050e2c0a113
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-08 11:46:04 -0500

    Add example for sending SMS via Amazon SNS

---
+++ apis/aws/send_sms.py
@@ -0,0 +1,19 @@
+"""
+To successfully run this script, you must put the proper credentials in
+~/.aws/credentials and give the user with those credentials the appropriate
+permissions. For example, go to the user's Permissions tab in IAM and attach the
+AmazonSNSFullAccess policy.
+
+"""
+
+from pprint import pprint
+import os
+import boto3
+
+client = boto3.client('sns')
+
+resp = client.publish(
+    PhoneNumber=os.environ['SMS_NUMBER'],
+    Message='This message was sent via the API',
+)
+pprint(resp)
