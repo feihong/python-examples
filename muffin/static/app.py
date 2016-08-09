8dc7e4d9d1d4f68c48e55776663d15f976202044
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-08 20:21:08 -0500

    Add web sockets and a long-running async task

--- muffin/static/app.py
+++ muffin/static/app.py
@@ -1,4 +1,41 @@
-from muffin_example import ExampleApplication
+import json
+import asyncio
+import muffin
+from muffin_example import ExampleApplication, WebSocketWriter
 
 
 app = ExampleApplication()
+
+
+@app.register('/websocket/')
+async def websocket(request):
+    ws = muffin.WebSocketResponse()
+    await ws.prepare(request)
+    print('Websocket opened')
+
+    task = None
+
+    async for msg in ws:
+        print(msg)
+        if msg.data == 'start' and not task:
+            coroutine = long_task(WebSocketWriter(ws))
+            task = asyncio.ensure_future(coroutine)
+            def done(future):
+                nonlocal task
+                task = None
+            task.add_done_callback(done)
+        elif msg.data == 'stop' and task:
+            task.cancel()
+            task = None
+
+    await ws.close()
+    print('Websocket closed')
+
+    return ws
+
+
+async def long_task(writer):
+    total = 150
+    for i in range(1, total+1):
+        writer.write(type='progress', value=i, total=total)
+        await asyncio.sleep(0.05)
