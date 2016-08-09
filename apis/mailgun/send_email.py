ee3d20b4235c4c290d6dd35a408d67ef16542f6b
Feihong <feihong.hsu@gmail.com>
2016-08-09 17:24:59 -0500

    Add mailgun example for sending email

---
+++ apis/mailgun/send_email.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import datetime
+from pprint import pprint
+import requests
+
+
+recipient = sys.argv[1]
+
+domain, private_key = os.environ['MAILGUN_PARAMS'].split(';')
+
+url = 'https://api.mailgun.net/v3/{domain}/messages'.format(domain=domain)
+
+resp = requests.post(
+    url,
+    auth=('api', private_key),
+    data={
+        'from': 'Overlord <overlord@{}>'.format(domain),
+        'to': recipient,
+        'subject': 'Hello from Mailgun',
+        'text': 'This email was sent at {:%I:%M}'.format(datetime.datetime.now()),
+    },
+)
+pprint(resp.text)
