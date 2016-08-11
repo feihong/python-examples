5b3d7d759bca20112d832e990a06dc6479f6c053
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-11 10:01:37 -0500

    Print the return code of the finished subprocess

--- asyncio/create_subprocess_exec.py
+++ asyncio/create_subprocess_exec.py
@@ -7,16 +7,17 @@ async def main():
         await asyncio.sleep(1)
         print(i)
 
-        # if i == 3:
-        #     asyncio.ensure_future(run_subprocess())
+        if i == 3:
+            asyncio.ensure_future(run_subprocess())
 
 
 async def run_subprocess():
     print('Starting subprocess')
-    code = 'import time; time.sleep(3)'
-    proc = await asyncio.create_subprocess_exec('python', '-c', code)
-    await proc.wait()
-    print('Subprocess finished!')
+    code = 'import time, datetime; time.sleep(3)'
+    proc = await asyncio.create_subprocess_exec(
+        'python', '-c', code, stdout=asyncio.subprocess.PIPE)
+    return_code = await proc.wait()
+    print('Subprocess finished with return code {}'.format(return_code))
 
 
 loop = asyncio.get_event_loop()
