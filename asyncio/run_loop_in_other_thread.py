f1b75d1ab594ecf0d82c4558ca5db75615596494
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-23 14:41:08 -0500

    Stop a blocking coroutine from another thread

--- asyncio/run_loop_in_other_thread.py
+++ asyncio/run_loop_in_other_thread.py
@@ -3,11 +3,18 @@ import threading
 import asyncio
 
 
-def run_event_loop(loop, stop_event):
-    loop.run_until_complete(loop_main(stop_event))
+def run_event_loop():
+    global loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(loop_main())
 
 
-async def loop_main(stop_event):
+async def loop_main():
+    global stop_event
+    stop_event = asyncio.Event()
+
+    print('Event loop is running inside: ' + threading.current_thread().name)
     for i in range(10):
         print(i)
         asyncio.sleep(0.1)
@@ -16,13 +23,16 @@ async def loop_main(stop_event):
 
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    stop_event = asyncio.Event()
+    loop = None
+    stop_event = None
 
-    thread = threading.Thread(target=run_event_loop, args=[loop, stop_event])
+    thread = threading.Thread(target=run_event_loop)
     thread.start()
     print('Sleep...')
     time.sleep(3)
     print('Awoke')
+
+    # Set the stop event to make the loop coroutine stop blocking.
     loop.call_soon_threadsafe(stop_event.set)
+
     thread.join()
