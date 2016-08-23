60c6757efa4a1a21c3c19bebf8a5c574676b292c
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-23 14:01:17 -0500

    Try to stop the coroutine in another thread by setting asyncio.Event

--- asyncio/run_loop_in_other_thread.py
+++ asyncio/run_loop_in_other_thread.py
@@ -1,19 +1,28 @@
+import time
 import threading
 import asyncio
 
 
-def run_event_loop():
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(count(10))
+def run_event_loop(loop, stop_event):
+    loop.run_until_complete(loop_main(stop_event))
 
 
-async def count(n):
-    for i in range(n):
+async def loop_main(stop_event):
+    for i in range(10):
         print(i)
         asyncio.sleep(0.1)
 
+    await stop_event.wait()
+
 
 if __name__ == '__main__':
-    thread = threading.Thread(target=run_event_loop)
+    loop = asyncio.new_event_loop()
+    stop_event = asyncio.Event()
+
+    thread = threading.Thread(target=run_event_loop, args=[loop, stop_event])
     thread.start()
+    print('Sleep...')
+    time.sleep(3)
+    print('Awoke')
+    loop.call_soon_threadsafe(stop_event.set)
     thread.join()
