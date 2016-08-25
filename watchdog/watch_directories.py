47c115695cec48d047cfeda90840163cf9bd20db
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-25 10:37:01 -0500

    Print out current thread name in watchdog example

--- watchdog/watch_directories.py
+++ watchdog/watch_directories.py
@@ -1,11 +1,15 @@
+import threading
 from pathlib import Path
 import time
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
 class MyEventHandler(FileSystemEventHandler):
     def dispatch(self, evt):
+        thread = threading.current_thread()
+        print('Inside thread %s' % thread.name)
         if not evt.is_directory:
             print('%s: %s' % (evt.event_type, evt.src_path))
 
@@ -14,9 +18,9 @@ if __name__ == '__main__':
     event_handler = MyEventHandler()
     observer = Observer()
     observer.schedule(
-        event_handler, str(Path('.').parent / 'aiohttp'), recursive=True)
+        event_handler, str(Path('..') / 'aiohttp'), recursive=True)
     observer.schedule(
-        event_handler, str(Path('.').parent / 'tornado'), recursive=True)
+        event_handler, str(Path('..') / 'tornado'), recursive=True)
     observer.start()
     try:
         while True:
