b266b260c76d51e11519219ce0a7984a8f96272d
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-04 17:17:20 -0500

    Add a GetItem example that shows how to retrieve item specific values

---
+++ apis/ebay/show_item_specifics.py
@@ -0,0 +1,27 @@
+import json
+import os
+from ebaysdk.trading import Connection as Trading
+
+
+credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
+api = Trading(config_file=None, **credentials)
+
+response = api.execute('GetItem', {
+    'ItemID': 181926350758,
+    'IncludeItemSpecifics': True,
+    'OutputSelector': [
+        'Item.ItemSpecifics.NameValueList',
+        'Item.ShippingPackageDetails',
+    ]
+})
+with open('item.json', 'w') as fp:
+    json.dump(response.dict(), fp, indent=2)
+
+item = response.reply.Item
+
+spd = item.ShippingPackageDetails
+print('{} lb, {} oz'.format(spd.WeightMajor.value, spd.WeightMinor.value))
+
+item_specifics = item.ItemSpecifics.NameValueList
+for spec in item_specifics:
+    print('- %s: %s' % (spec.Name, spec.Value))
