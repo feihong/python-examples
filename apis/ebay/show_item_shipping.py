12d91f6f728cb558aee4d9c149e3fb99173653b8
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-06 12:37:57 -0500

    Ugh, fix terrible merge commit

--- apis/ebay/show_item_shipping.py
+++ apis/ebay/show_item_shipping.py
@@ -6,8 +6,8 @@ from ebaysdk.trading import Connection as Trading
 credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
 api = Trading(config_file=None, **credentials)
 
-response = api.execute('GetItem', {'ItemID': 181926350758})
-<<<<<<< HEAD
+response = api.execute('GetItem', {'ItemID': 182232608407})
+
 with open('item.json', 'w') as fp:
     json.dump(response.dict(), fp, indent=2)
 
@@ -16,6 +16,8 @@ print(item.Title)
 
 sd = item.ShippingDetails
 
+# Possible values: USPSFirstClass, USPSPriorityFlatRateEnvelope,
+# USPSPriorityMailPaddedFlatRateEnvelope, ShippingMethodStandard, ...
 print('Domestic shipping service: {}'.format(
     sd.ShippingServiceOptions.ShippingService
 ))
@@ -30,13 +32,3 @@ print('{} lb, {} oz'.format(spd.WeightMajor.value, spd.WeightMinor.value))
 
 weight = int(spd.WeightMajor.value) *  16 + int(spd.WeightMinor.value)
 print('Weight in oz: {}'.format(weight))
-=======
-# pprint(response.dict())
-details = response.reply.Item.ShippingPackageDetails
-print(details)
-
-lb = int(details.WeightMajor.value)
-oz = int(details.WeightMinor.value)
-
-print('Item weights %d oz' % (16 * lb + oz))
->>>>>>> origin/master
