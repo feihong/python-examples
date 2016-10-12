0f3162b041f122dc21d79cd00c0d9abcbe96f808
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-12 10:30:19 -0500

    Print out rates for parcels of several different weights

--- apis/postmen/calculate_rates.py
+++ apis/postmen/calculate_rates.py
@@ -13,25 +13,9 @@ from postmen import Postmen, PostmenException
 api_key, shipper_id = os.environ['POSTMEN_PARAMS'].split(';')
 region = 'sandbox'
 
+# In ounces
+weights = [1, 2, 3, 4, 5]
 
-WEIGHT_IN_OZ = 2
-
-
-item = dict(
-    description='Snack Bar',
-    hs_code='11111111',
-    origin_country='CHN',
-    price={
-        'amount': 13.50,
-        'currency': 'USD'
-    },
-    quantity=1,
-    sku='111-222-333',
-    weight={
-        'unit': 'lb',
-        'value': WEIGHT_IN_OZ / 16,
-    }
-)
 
 sender = dict(
     contact_name='Hugo Strongola',
@@ -68,45 +52,64 @@ box_type = 'usps_parcel'
 # box_type = 'usps_flat_rate_envelope'
 # box_type = 'usps_flat_rate_padded_envelope'
 
-parcel = dict(
-    box_type=box_type,
-    weight=item['weight'],
-    dimension={
-        'width': 7,
-        'height': 5,
-        'depth': 0.2,
-        'unit': 'in'
-    },
-    items=[
-         item
-    ]
-)
+def get_payload(weight):
+    item = dict(
+        description='Snack Bar',
+        hs_code='11111111',
+        origin_country='CHN',
+        price={
+            'amount': 13.50,
+            'currency': 'USD'
+        },
+        quantity=1,
+        sku='111-222-333',
+        weight={
+            'unit': 'lb',
+            'value': weight / 16,
+        }
+    )
 
-payload = dict(
-    is_document=False,
-    async=False,
-    shipper_accounts=[{'id': shipper_id}],
-    shipment={
-        'parcels': [parcel],
-        'ship_from': sender,
-        'ship_to': receiver,
-    }
-)
+    parcel = dict(
+        box_type=box_type,
+        weight=item['weight'],
+        dimension={
+            'width': 7,
+            'height': 5,
+            'depth': 0.2,
+            'unit': 'in'
+        },
+        items=[
+             item
+        ]
+    )
+
+    payload = dict(
+        is_document=False,
+        async=False,
+        shipper_accounts=[{'id': shipper_id}],
+        shipment={
+            'parcels': [parcel],
+            'ship_from': sender,
+            'ship_to': receiver,
+        }
+    )
+    return payload
 
 
 print('Box type:', box_type)
-print('Weight in oz:', WEIGHT_IN_OZ)
 print()
 
-try:
-    api = Postmen(api_key, region)
-    result = api.create('rates', payload)
-    with open('result.json', 'w') as fp:
-        json.dump(result, fp, indent=2)
-
-    for rate in result['rates']:
-        print(rate['service_type'], rate['total_charge']['amount'])
-except PostmenException as ex:
-    print(ex.code())
-    print(ex.message())
-    print(ex.details())
+for weight in weights:
+    print('Weight: {} oz'.format(weight))
+    try:
+        api = Postmen(api_key, region)
+        result = api.create('rates', get_payload(weight))
+        # with open('result.json', 'w') as fp:
+        #     json.dump(result, fp, indent=2)
+        for rate in result['rates']:
+            print(rate['service_type'], rate['total_charge']['amount'])
+        print('-'*75)
+    except PostmenException as ex:
+        print(ex.code())
+        print(ex.message())
+        print(ex.details())
