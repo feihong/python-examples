a747c886217b5952a698ccbb1bf85cecb2d246dc
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-15 21:48:09 -0500

    Add example where numbers are drawn in the corners of the page

---
+++ reportlab/draw_in_corners.py
@@ -0,0 +1,19 @@
+"""
+Draw numbers in the four corners of the page
+
+"""
+from reportlab.pdfgen.canvas import Canvas
+
+
+inch = 72
+
+
+with open('output.pdf', 'wb') as fp:
+    c = Canvas(fp, pagesize=(8.5*inch, 11*inch))
+
+    c.setFont('Times-Roman', 10)
+    c.drawString(0, 780, '1')
+    c.drawString(605, 780, '2')
+    c.drawString(605, 5, '3')
+    c.drawString(0, 5, '4')
+    c.save()
