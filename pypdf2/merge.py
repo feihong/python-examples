dd1e14cccec0a76b00ddb43eb13d3f302863338e
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-15 21:40:49 -0500

    Put 3 labels side by side

--- pypdf2/merge.py
+++ pypdf2/merge.py
@@ -20,18 +20,41 @@ def get_new_page():
     return reader.getPage(0)
 
 
-def merge_label(page):
+def add_label(page, x, y):
     fp = open('label.pdf', 'rb')
     reader = PdfFileReader(fp)
-    page.mergeTranslatedPage(reader.getPage(0), inch, 2*inch)
-    
+    page.mergeTranslatedPage(reader.getPage(0), x, y)
+
+
+def add_text(page):
+    items = '4-dork-9, 3-dweeb-5'
+
+    bio = BytesIO()
+    c = Canvas(bio, pagesize=(11*inch, 8.5*inch))
+    c.setFont('Helvetica', 12)
+    c.drawString(inch, 120, 'User: CoolioFoolio')
+    c.drawString(inch, 100, 'Tracking number: 0000 1111 2222 3333 4444 5555 66')
+    c.drawString(inch, 80, 'Items: ' + items)
+
+    c.setFont('Helvetica', 9)
+    c.translate(250, 450)
+    c.rotate(-90)
+    c.drawString(0, 0, items)
+    c.save()
+    reader = PdfFileReader(bio)
+    page.mergePage(reader.getPage(0))
+
 
 if __name__ == '__main__':
     output = PdfFileWriter()
     page = get_new_page()
     output.addPage(page)
 
-    merge_label(page)
+    add_label(page, -10, 2*inch)
+    add_label(page, 253, 2*inch)
+    add_label(page, 515, 2*inch)
+
+    add_text(page)
 
     with open('output.pdf', 'wb') as fout:
         output.write(fout)
