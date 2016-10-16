2487b9a1abe5b79f67abe6ac8f94e9314b4c415a
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-16 17:27:22 -0500

    Add a pdf crop example

---
+++ pypdf2/crop.py
@@ -0,0 +1,29 @@
+"""
+Crop the contents of label.pdf and output the results to cropped_label.pdf.
+
+Source: http://stackoverflow.com/questions/457207/cropping-pages-of-a-pdf-file
+
+"""
+import datetime
+from io import BytesIO
+from reportlab.pdfgen.canvas import Canvas
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
+def get_label_page():
+    fp = open('label.pdf', 'rb')
+    reader = PdfFileReader(fp)
+    page = reader.getPage(0)
+    page.cropBox.lowerLeft = (15, 10)
+    page.cropBox.upperRight = (273, 457)
+    # page.trimBox.lowerLeft = (15, 10)
+    # page.trimBox.upperRight = (273, 457)
+    return page
+
+
+if __name__ == '__main__':
+    output = PdfFileWriter()
+    output.addPage(get_label_page())
+
+    with open('cropped_label.pdf', 'wb') as fout:
+        output.write(fout)
