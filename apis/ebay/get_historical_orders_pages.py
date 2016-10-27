b37be5ec1a94e25751bfa03c1c22870bd7541a93
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-27 14:39:25 -0500

    Download all pages of the GetOrders response

---
+++ apis/ebay/get_historical_orders_pages.py
@@ -0,0 +1,62 @@
+"""
+Fetch historical orders based on a datetime range, but exclude most fields in
+the response.
+
+"""
+import os
+import datetime
+import json
+import itertools
+import arrow
+from ebaysdk.trading import Connection as Trading
+
+
+credentials = dict(zip(('appid', 'devid', 'certid', 'token'), os.environ['EBAY_PARAMS'].split(';')))
+api = Trading(config_file=None, **credentials)
+
+start = arrow.utcnow().replace(days=-120).replace(
+    hour=0, minute=0, second=0, microsecond=0)
+end = start.replace(days=+30)
+
+def get_orders(page):
+    response = api.execute('GetOrders', {
+        'CreateTimeFrom': start,
+        'CreateTimeTo': end,
+        'OrderStatus': 'Completed',
+        'Pagination': {
+            'PageNumber': page,
+            'EntriesPerPage': 100,
+        },
+        'OutputSelector': [
+            'PageNumber',
+            'PaginationResult',
+            'ReturnedOrderCountActual',
+            'OrderArray.Order.TransactionArray.Transaction.Item.ItemID',
+        ]
+    })
+    if page == 1:
+        pagination = response.reply.PaginationResult
+        print('Found {} orders over {} pages'.format(
+            pagination.TotalNumberOfEntries, pagination.TotalNumberOfPages))
+    # with open('orders.json', 'w') as fp:
+    #     json.dump(response.dict(), fp, indent=2)
+    return response
+
+
+def get_all_orders():
+    for page in itertools.count(1):
+        response = get_orders(page)
+        reply = response.reply
+        print('Page {}, {} items'.format(page, reply.ReturnedOrderCountActual))
+        orders = response.dict()['OrderArray']['Order']
+        for order in orders:
+            yield order
+        if reply.PageNumber == reply.PaginationResult.TotalNumberOfPages:
+            break
+
+
+if __name__ == '__main__':
+    print('Search range: {:YYYY-MM-DD} to {:YYYY-MM-DD}'.format(start, end))
+    orders = list(get_all_orders())
+    with open('orders.json', 'w') as fp:
+        json.dump(orders, fp, indent=2)
