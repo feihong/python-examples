d028d83b23dad6466635742ce53c1c315ebb0855
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-28 18:44:43 -0500

    Add useful docstrings and comments

--- apis/easypost/create_foreign_label.py
+++ apis/easypost/create_foreign_label.py
@@ -5,6 +5,13 @@ Customs guide: https://www.easypost.com/customs-guide.html
 
 EasyPost's foreign-bound labels are 1755 × 1155.
 
+You can visit http://hts.usitc.gov/ to search for valid values for the
+hs_tariff_number field. You only need to pass the first six digits, without the
+periods.
+
+If the value of the goods is less than $2,500, then the value of the eel_pfc
+field should be "NOEEI 30.37(a)"
+
 """
 import webbrowser
 import os
@@ -47,22 +54,22 @@ parcel = easypost.Parcel.create(
 
 customs_item = easypost.CustomsItem.create(
     description = 'EasyPost Mug',
-    hs_tariff_number = 123456,
-    origin_country='US',
+    origin_country='CN',
     quantity=1,
     value=25.45,
     weight=weight,
+    hs_tariff_number=820770,
 )
 
 customs_info = easypost.CustomsInfo.create(
-    customs_certify=1,
-    customs_signer='Hector Hammerfall',
+    customs_certify=True,       # should always be True
+    customs_signer='Hector Hammerfist',
     contents_type='gift',
-    contents_explanation='',
+    contents_explanation=None,  # only needed if contents_type is 'other'
     eel_pfc='NOEEI 30.37(a)',
     non_delivery_option='return',
     restriction_type='none',
-    restriction_comments='',
+    restriction_comments=None,  # only needed if restriction_type is 'other'
     customs_items=[customs_item]
 )
 
