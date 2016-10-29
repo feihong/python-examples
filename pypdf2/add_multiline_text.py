68894c587894a4a554255bbb9dc254369363533f
Feihong Hsu <feihong.hsu@gmail.com>
2016-10-29 17:43:45 -0500

    Add multiline text example

---
+++ pypdf2/add_multiline_text.py
@@ -0,0 +1,37 @@
+import datetime
+from io import BytesIO
+import textwrap
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.colors import HexColor
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
+chunk = '0123456789 '
+text = 'Tiger, tiger, burning bright\nIn the forests of the night'
+
+
+def get_text_page():
+    inch = 72
+    bio = BytesIO()
+    c = Canvas(bio, pagesize=(11*inch, 8.5*inch))
+
+    x, y = 50, 50
+    c.drawImage('label.png', x, y, 4*inch, 6*inch)
+    c.rect(x, y, 4*inch, 6*inch)
+
+    t = c.beginText()
+    t.setFont('Courier', 8)
+    t.setTextOrigin(55, 190)
+    t.textLines(text)
+    c.drawText(t)
+
+    c.save()
+    return PdfFileReader(bio).getPage(0)
+
+
+if __name__ == '__main__':
+    writer = PdfFileWriter()
+    writer.addPage(get_text_page())
+
+    with open('output.pdf', 'wb') as fp:
+        writer.write(fp)
