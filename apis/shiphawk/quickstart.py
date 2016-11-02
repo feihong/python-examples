37eb450e65fd7e1bf802dcd3ba909899e8d0085c
Feihong <feihong.hsu@gmail.com>
2016-11-02 18:34:15 -0500

    Print out the rates in a nice, readable way

--- apis/shiphawk/quickstart.py
+++ apis/shiphawk/quickstart.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 
 
@@ -7,19 +8,21 @@ api_key = os.environ['SHIPHAWK_API_KEY']
 
 url = 'https://sandbox.shiphawk.com/api/v4/rates?api_key=' + api_key
 
-data = """{
-  "items":[
+data = {
+  'items':[
     {
-      "item_type": "parcel",
-      "length": "10",
-      "width" : "10",
-      "height": "11",
-      "weight": "10",
-      "value": 100.00
+      'item_type': 'parcel',
+      'length': '5',
+      'width' : '5',
+      'height': '2',
+      'weight': '1',
+      'value': 15.00
     }
   ],
-  "origin_address":{ "zip": "93101"},
-  "destination_address":{ "zip": "60060"}
-}"""
-response = requests.post(url, data)
-print(response.text)
+  'origin_address':{ 'zip': '93101'},
+  'destination_address':{ 'zip': '60060'}
+}
+response = requests.post(url, json.dumps(data))
+# print(json.dumps(response.json(), indent=2))
+for rate in response.json()['rates']:
+    print('{carrier} {service_level}: {price}'.format(**rate))
