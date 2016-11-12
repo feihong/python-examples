3e748188b4d79965732cd5fb70cb578c25ac1358
Feihong Hsu <feihong.hsu@gmail.com>
2016-11-11 20:58:50 -0600

    Add an example of drawing centered text

---
+++ svgwrite/draw_centered_text.py
@@ -0,0 +1,14 @@
+from svgwrite import Drawing, rgb
+
+color1 = '#ff5722'
+color2 = '#222'
+
+width, height = 800, 200
+dwg = Drawing('test.svg', size=(width, height))
+dwg.add(dwg.rect(insert=(0, 0), size=(width, height), fill=color2))
+dwg.add(dwg.text(
+    '骑虎难下',
+    fill=color1,
+    insert=(width/2, height/2 + 30), text_anchor='middle', font_size=80
+))
+dwg.save()
