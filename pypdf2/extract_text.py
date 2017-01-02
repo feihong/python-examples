19eaf1c794fd5b0ebf8eed447b92a539ba0053d6
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-02 17:23:53 -0600

    Try to extract text from eBay labels pdf

--- pypdf2/extract_text.py
+++ pypdf2/extract_text.py
@@ -1,6 +1,11 @@
+import os
 from PyPDF2 import PdfFileReader
 
+# path = 'input.pdf'
 
-reader = PdfFileReader('input.pdf')
+# Probably will not be able to extract text from this document.
+path = os.environ['PRIVATE_DATA'] + '/ebay/12-labels.pdf'
+
+reader = PdfFileReader(path)
 page = reader.getPage(0)
 print(page.extractText())
