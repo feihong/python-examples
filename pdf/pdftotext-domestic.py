ee71d1e2c32222f29f3d4aaf1c1fc6033166314b
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-02 22:31:31 -0600

    Refactor example to grab the tracking numbers for domestic labels

--- pdf/pdftotext-domestic.py
+++ pdf/pdftotext-domestic.py
@@ -1,7 +1,10 @@
 """
-Grab the textual content from a two-label page. Note that if we just grab the
-text from the entire page, the text will be out of order because the labels
-are sideways.
+Grab the tracking numbers content from a page that contains two domestic labels.
+
+Note that if we just grab the text from the entire page, the text will be out of order because the labels are sideways.
+
+Running this script on the 7th page (containing the foreign label) won't grab
+any numbers at all.
 
 """
 import os
@@ -9,17 +12,17 @@ import sys
 import subprocess
 from PyPDF2 import PdfFileReader
 
-# Probably will not be able to extract text from this document.
-path = os.environ['PRIVATE_DATA'] + '/ebay/12-labels.pdf'
 
+path = os.environ['PRIVATE_DATA'] + '/ebay/12-labels.pdf'
 page = sys.argv[1]
+
 
 def main():
     # Top label.
-    print(get_text_for_rect(90, 45, 460, 300))
+    print(get_text_for_rect(140, 95, 30, 195))
     print('='*75)
     # Bottom label.
-    print(get_text_for_rect(90, 445, 460, 300))
+    print(get_text_for_rect(140, 495, 30, 195))
 
 
 def get_text_for_rect(x, y, w, h):
