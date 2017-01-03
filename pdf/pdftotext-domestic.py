fee2a5b18cb3fb3e94b9be2d924b272eaa30cef9
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-02 23:03:54 -0600

    Add regex matching

--- pdf/pdftotext-domestic.py
+++ pdf/pdftotext-domestic.py
@@ -10,31 +10,49 @@ any numbers at all.
 import os
 import sys
 import subprocess
+import re
 from PyPDF2 import PdfFileReader
 
 
 path = os.environ['PRIVATE_DATA'] + '/ebay/12-labels.pdf'
 page = sys.argv[1]
 
+TOP_BBOX = 140, 95, 30, 195
+BOTTOM_BBOX = 140, 495, 30, 195
+
 
 def main():
-    # Top label.
-    print(get_text_for_rect(140, 95, 30, 195))
-    print('='*75)
-    # Bottom label.
-    print(get_text_for_rect(140, 495, 30, 195))
+    for tn in get_tracking_numbers():
+        print(tn)
 
 
-def get_text_for_rect(x, y, w, h):
+def get_tracking_numbers():
+    result = []
+    for bbox in (TOP_BBOX, BOTTOM_BBOX):
+        text = get_tracking_number(bbox)
+        if re.match(r'\d{22}', text):
+            result.append(text)
+    return result
+
+
+
+def get_tracking_number(bbox):
+    result = get_text_for_bbox(*bbox)
+    return result.strip().replace(' ', '')
+
+
+def get_text_for_bbox(x, y, w, h):
     cmd = [
         'pdftotext',
         str(path),
         '-f', page,    # first page
         '-l', page,    # last page
+        # Crop parameters
         '-x', str(x),
         '-y', str(y),
         '-W', str(w),
         '-H', str(h),
+        # Send output to stdout
         '-'
     ]
     return subprocess.check_output(cmd).decode('utf-8')
