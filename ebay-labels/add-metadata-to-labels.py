ab9ff6410a4d424ba5dd3e838d4f87a7b621dad7
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-01 13:20:42 -0600

    Add example text for username and page number

--- ebay-labels/add-metadata-to-labels.py
+++ ebay-labels/add-metadata-to-labels.py
@@ -24,19 +24,24 @@ foreign = textwrap.fill(chunk * 10, 23)
 domestic_notes = textwrap.fill(chunk * 24, 73)
 foreign_notes = textwrap.fill(chunk * 24, 79)
 center_line = '-  ' * 30
+user = 'User: blarney-stone-bear-9381'
+
 
 def main():
     reader = PdfFileReader(open(path, 'rb'))
     pages = [reader.getPage(i) for i in range(reader.numPages)]
 
     # Write pages to output file.
-    writer = writer = PdfFileWriter()
-    for page in pages[:5]:
+    writer = PdfFileWriter()
+    for i, page in enumerate(pages[:5], 1):
         add_message(page, content=domestic, translate=(244, 316))
         add_message(page, content=domestic_notes, translate=(100, 347))
         add_message(page, content=center_line, translate=(45, 397))
         add_message(page, content=domestic, translate=(244, 713))
         add_message(page, content=domestic_notes, translate=(100, 425))
+        add_message(page, content=user, translate=(45, 760))
+        add_message(page, content='Page 10{} of 800'.format(i), translate=(470, 760))
+
         writer.addPage(page)
 
     page6 = pages[5]
@@ -55,7 +60,7 @@ def main():
 def add_message(page, content, translate, rotate=0):
     inch = 72
     buf = BytesIO()
-    c = Canvas(buf, pagesize=(11*inch, 8.5*inch))
+    c = Canvas(buf, pagesize=(8.5*inch, 11*inch))
 
     x, y = translate
     y = 11*inch - y
