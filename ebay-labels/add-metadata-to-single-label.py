498bcaf99386d0c233223ad2b7a1864668ecb51f
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-13 18:34:04 -0500

    Add bordered, filled rectangle and notes

--- ebay-labels/add-metadata-to-single-label.py
+++ ebay-labels/add-metadata-to-single-label.py
@@ -1,21 +1,21 @@
 import os
 import textwrap
-from reportlab.pdfgen.canvas import Canvas
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 from common import add_message, add_rect
 
 
 path = os.environ['PRIVATE_DATA'] + '/ebay/single-domestic.pdf'
-domestic = textwrap.fill('M' * 28 * 2, 28)
-
+packing = textwrap.fill('M' * 28 * 2, 28)
+notes = textwrap.fill('M' * 83 * 4, 83)
 
 reader = PdfFileReader(open(path, 'rb'), strict=False)
 page1 = reader.getPage(0)
 
 writer = PdfFileWriter()
-add_message(page1, content=domestic, translate=(223, 318))
+add_message(page1, content=packing, translate=(223, 318))
 add_rect(page1, (25, 340, 505, 49))
+add_message(page1, content=notes, translate=(29, 350))
 
 writer.addPage(page1)
 
--- ebay-labels/common.py
+++ ebay-labels/common.py
@@ -1,5 +1,7 @@
 from io import BytesIO
+
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.colors import white
 from PyPDF2 import PdfFileReader
 
 
@@ -29,11 +31,18 @@ def add_message(page, content, translate, rotate=0):
 
 
 def add_rect(page, bbox):
+    """
+    Add a white-filled rectangle with dashed border to the given page.
+
+    """
     buf = BytesIO()
     canvas = Canvas(buf, pagesize=(8.5*inch, 11*inch))
     x, y, w, h = bbox
     y = 11*inch - y - h
-    canvas.rect(x, y, w, h)
+    canvas.setFillColor(white)
+    canvas.setLineWidth(0.1)
+    canvas.setDash(3, 6)
+    canvas.roundRect(x, y, w, h, 5, fill=True)
     canvas.save()
     new_page = PdfFileReader(buf).getPage(0)
     page.mergePage(new_page)
